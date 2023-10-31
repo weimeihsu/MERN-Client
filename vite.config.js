23cf615b143499e935ac7b2eb3a92356c573251f
weimeihsu <weimeihsu@users.noreply.github.com>
2023-10-31 11:19:10 +0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      // string shorthand: http://localhost:5173/foo -> http://localhost:4567/foo
       '/api/records': 'http://localhost:8080/',
 
       // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
