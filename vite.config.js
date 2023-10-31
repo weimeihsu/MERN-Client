7914c616a2023d6a72b77d06bb571404ae5db2fc
weimeihsu <weimeihsu@users.noreply.github.com>
2023-10-31 12:06:42 +0800

    vite config

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       // string shorthand: http://localhost:5173/foo -> http://localhost:4567/foo
-      '/api/records': 'http://localhost:8080/',
-
-      // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
-      '/api': {
-        target: 'https://mern-weimei-backend.onrender.com/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+      '/api': 'http://localhost:8080/',
+    }
   }
 })
