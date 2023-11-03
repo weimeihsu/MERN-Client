e5fbc7d226f761e86f2b8b173fc29e05dc9ba0e6
weimeihsu <weimeihsu@users.noreply.github.com>
2023-11-03 16:26:02 +0800

    remove homepage consolelog

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import Container from '@mui/material/Container'
 const HomePage = () => {
-    console.log(import.meta.env.VITE_REACT_APP_SERVER_URL)
     return (
         <Container>
              <h1>homepage</h1>
